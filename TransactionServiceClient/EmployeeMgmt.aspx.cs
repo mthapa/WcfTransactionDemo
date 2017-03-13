1341a93f6526013c4f1fa35363d415dfb5183028
Madan Thapa <MadanT@a-t-g.com>
2017-03-13 16:15:45 -0500

    First Commit

---
+++ TransactionServiceClient/EmployeeMgmt.aspx.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using TransactionServiceClient.TransactionServiceReference;
+using System.Transactions;
+using System.Data.SqlClient;
+
+namespace TransactionServiceClient
+{
+	public partial class EmployeeMgmt : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+			if (!Page.IsPostBack)
+			{
+				txtEmployeeName.Text = "";
+				txtEmployeeSalary.Text = "";
+				txtStatus.Text = "";
+			}
+
+		}
+
+		protected void btnCreate_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				var TS = new EmployeeSalaryClient();
+				using (TransactionScope Trs = new TransactionScope())
+				{
+					int id = TS.CreateEmployee(new Employee() { EName = txtEmployeeName.Text, ESalary = double.Parse(txtEmployeeSalary.Text) });
+					if (id != 0)
+					{
+						TS.CreateSalaryHistory(new SalaryHistory() { Eid = id, ESalary = double.Parse(txtEmployeeSalary.Text), StDate = DateTime.Now, EdDate = null });
+					}
+
+					//Log to the client side
+					CreateTransactionLog(new Transaction { TDate = DateTime.Now, TInfo = String.Format("Name:{0},Salary:{1} ", txtEmployeeName.Text, txtEmployeeSalary.Text) });
+
+					Trs.Complete();
+				}
+
+				txtStatus.Text = String.Format("Success.");
+
+			}
+			catch (Exception ex)
+			{
+
+				txtStatus.Text = String.Format("Error {0}", ex.Message);
+			}
+
+		}
+
+
+		public void CreateTransactionLog(Transaction tlog)
+		{
+			var con = new SqlConnection("Data Source=MTHAPA\\MTHAPA;Initial Catalog=TransactionDb;Integrated Security=True");
+			var cmd = new SqlCommand("insert into Transactions1 (TInfo, TDate) values (@TInfo, @TDate)", con);
+			cmd.Parameters.AddWithValue("@TInfo", tlog.TInfo);
+			cmd.Parameters.AddWithValue("@TDate", tlog.TDate);
+			con.Open();
+			cmd.ExecuteNonQuery();
+			con.Close();
+		}
+
+
+
+	}
+
+	public class Transaction
+	{
+		public int Tid { get; set; }
+		public String TInfo { get; set; }
+		public DateTime TDate { get; set; }
+
+	}
+}
---
+++ TransactionServiceLibrary/IEmployeeSalary.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Transactions;
+namespace TransactionServiceLibrary
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IEmployeeSalary
+    {
+        [OperationContract] 
+        [TransactionFlow(TransactionFlowOption.Mandatory)]
+        int CreateEmployee(Employee E);
+
+
+        [OperationContract] 
+        [TransactionFlow(TransactionFlowOption.Mandatory)]
+        void CreateSalaryHistory(SalaryHistory SH);
+    }
+
+    [DataContract]
+    public class Employee
+    {
+        [DataMember]
+        public int Eid { get; set; }
+
+        [DataMember]
+        public string EName { get; set; }
+
+        [DataMember]
+        public double ESalary { get; set; }
+    }
+
+    [DataContract]
+    public class SalaryHistory
+    {
+        [DataMember]
+        public int SNo { get; set; }
+
+        [DataMember]
+        public int Eid { get; set; }
+
+        [DataMember]
+        public double ESalary { get; set; }
+
+        [DataMember]
+        public DateTime StDate { get; set; }
+
+        [DataMember]
+        public DateTime? EdDate { get; set; }
+
+    }
+}
+    	
+    
